1080afb328e425afd00dc77e372323cfebc1393f
Muhammad Rizal Fadhilah <rizaelapex@gmail.com>
2026-01-08 06:57:45 +0700

    docker fixx

--- backend/internal/config/database.go
+++ backend/internal/config/database.go
@@ -11,7 +11,16 @@ import (
 )
 
 func InitDB() *gorm.DB {
-	godotenv.Load()
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system environment variables")
+	}
+
+	log.Printf("Connecting to database: host=%s port=%s user=%s dbname=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"),
+	)
 
 	dsn := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
